9981af26400d845fcd71b384368ad0fe415252d6
Caroline Fassot <fassot.caroline@gmail.com>
2022-04-27 16:39:11 +0200

    Create layout

---
+++ layouts/page.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+	<Header/>
+	<main>
+		<Nuxt/>
+	</main>
+	<Footer/>
+</div>
+</template>
+
+<script>
+import Header from "~/components/layout/header/Header.vue"
+import Footer from "~/components/layout/footer/Footer.vue"
+export default {
+	name: "Layout",
+	components:{Header, Footer}
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,15 @@
 <template>
-  <div>
-    <Tutorial />
-		<Header></Header>
-    <p class="bg-lime-500">test</p>
-		<div class="test">
-				<p>test</p>
+	<div>
+		<div>
+			Page d'accueil
 		</div>
-		<Footer></Footer>
-  </div>
+	</div>
 </template>
 
 <script lang="ts">
-import Header from "~/components/layout/header/Header.vue";
-import Footer from "~/components/layout/footer/footer.vue";
 export default {
-    name: "IndexPage",
-    components: { Header, Footer }
+	name: 'IndexPage',
+	layout: "page",
 }
 </script>
 
@@ -23,7 +17,7 @@ export default {
 @import '../assets/styles/_variables.scss';
 .test {
 	p {
-		color:$mainBlue
+		color: $mainBlue;
 	}
 }
 </style>
