d0f925d3828295525bc291af4462d08c0a92fe93
Caroline Fassot <fassot.caroline@gmail.com>
2022-04-27 12:35:43 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Tutorial />
+    <p class="bg-lime-500">test</p>
+		<div class="test">
+				<p>test</p>
+		</div>
+	<p></p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss">
+.test {
+	p {
+		color:blue
+	}
+}
+</style>
