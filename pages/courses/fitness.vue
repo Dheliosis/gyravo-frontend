b01943066281c713080f29df6d8c90c17b2a5471
Caroline Fassot <fassot.caroline@gmail.com>
2022-05-03 11:10:20 +0200

    Create test pages

---
+++ pages/courses/fitness.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		hello
+	</div>
+</template>
+
+<script>
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "Fitness",
+	layout: "page"
+
+}
+</script>
+
+<style>
+
+</style>
