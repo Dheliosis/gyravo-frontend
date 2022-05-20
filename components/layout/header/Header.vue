d139620ff980d35c27cdb8793935ce79c2734ca1
Caroline Fassot <fassot.caroline@gmail.com>
2022-05-20 13:47:20 +0200

    Header

--- components/layout/header/Header.vue
+++ components/layout/header/Header.vue
@@ -1,11 +1,77 @@
 <template>
-	<header>Ceci est un header</header>
+	<header class="flex justify-between items-center">
+		<a href="/" class="logo">
+			<img src="~/static/logo_gyravo.png"/>
+		</a>
+
+		<div class="flex items-center">
+				<ul class="flex content-center">
+					<li>
+						<a href="/courses">
+							Les cours
+						</a>
+					</li>
+						<li>
+						<a href="/news">
+							Le blog
+						</a>
+					</li>
+					<li>
+						<a href="/contact">
+							Contact
+						</a>
+					</li>
+					<li>
+						<a href="/login" id="logIn">
+							Se connecter
+						</a>
+					</li>
+					<li>
+						<a href="/singup" id="signUp">
+							S'inscrire
+						</a>
+					</li>
+				</ul>
+
+				<img class="w-9 h-9 ml-3" src="../../../static/accessibilite.png">
+		</div>
+
+
+	</header>
 </template>
 
 <script>
 export default {
 	name: "Header",
+	data: () => {
+		return{
+		}
+	}
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../../../assets/styles/_variables.scss";
+header{
+	margin: 10px 2rem;
+}
+
+a{
+	color: $mainGreen;
+	margin: 0 1rem;
+}
+
+#signUp{
+	color: $mainViolet
+}
+
+#logIn{
+	color: $mainBlue
+}
+
+.logo{
+	width: 15rem;
+}
+
+
+</style>
