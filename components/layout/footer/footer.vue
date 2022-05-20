e7e9e6afd56dfee58be5534bc4a6988313525b53
Caroline Fassot <fassot.caroline@gmail.com>
2022-05-20 13:47:27 +0200

    Footer

--- components/layout/footer/footer.vue
+++ components/layout/footer/footer.vue
@@ -1,5 +1,41 @@
 <template>
-	<header>Ceci est un footer</header>
+	<footer class="flex justify-around items-center py-10">
+		<div>
+			<img class="w-40" src="../../../static/logo_court-monochrome_blanc.png">
+		</div>
+		<div class="flex align-center">
+			<div>
+				<p class="font-semibold mb-2">Plan du site</p>
+				<ul>
+					<li>
+						<a href="/">
+							Accueil
+						</a>
+					</li>
+				<li>
+						<a href="/courses">
+							Les cours
+						</a>
+					</li>
+				<li>
+						<a href="/news">
+							Le blog
+						</a>
+					</li>
+				<li>
+						<a href="/contact">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+		<div class="flex">
+			<img class="w-15 h-14 mr-2" src="../../../static/facebook.png">
+			<img class="w-15 h-14 ml-2" src="../../../static/youtube.png">
+		</div>
+	</footer>
 </template>
 
 <script>
@@ -8,4 +44,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../../../assets/styles/variables";
+footer{
+	color: white;
+	background-color: $mainViolet;
+}
+
+</style>
