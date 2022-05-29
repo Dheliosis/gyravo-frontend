0aae954f4e3236f4b455d0d0bdcfb82d06908a47
BlueFaste <fassot.caroline@gmail.com>
2022-05-29 14:39:06 +0200

    Create component Button

---
+++ components/Button.vue
@@ -0,0 +1,91 @@
+<template>
+	<button :class="`${backgroundColor} ${textColor} border-2 rounded-full px-8 py-2 font-bold`">
+		<i v-if="icon !== 'none'" :class="`fas ${icon}`" style="font-size:24px;"></i>
+		{{text}}
+		</button>
+</template>
+
+<script>
+export default {
+	props:{
+		text: {
+			type: String,
+			default: "Text button"
+		},
+		backgroundColor:{
+			type: String,
+			default: "white"
+		},
+		textColor:{
+			type: String,
+			default: "black"
+		},
+		icon:{
+			type: String,
+			default: "none"
+		}
+	},
+
+	created(){
+
+
+	}
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/_variables.scss";
+
+button{
+	transition: 0.5s;
+}
+
+.backgroundBlue{
+	background-color: $mainBlue;
+}
+
+.backgroundGreen{
+	background-color: $mainGreen;
+
+	&:hover{
+		background-color: $mainBlue;
+	}
+}
+
+.backgroundViolet{
+	background-color: $mainViolet;
+}
+
+.backgroundWhite{
+	background-color: $white;
+}
+
+.textBlue{
+	color: $mainBlue;
+	border-color: $mainBlue;
+	&:hover{
+		background-color: $secondaryBlue;
+	}
+}
+
+.textGreen{
+	color: $mainGreen;
+	border-color: $mainGreen;
+	&:hover{
+		background-color: $secondaryGreen;
+	}
+}
+
+.textViolet{
+	color: $mainViolet;
+	border-color: $mainViolet;
+}
+
+.textWhite{
+	color: $white;
+	border-color: $white;
+}
+
+
+</style>
