dd1be5b9848cea2a5c8ae1ade2ce945c0db3ba42
BlueFaste <fassot.caroline@gmail.com>
2022-05-29 09:49:52 +0200

    Index WiP

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
 	<div class="home">
 		<div class="home__sign text-white">
 			<div class="flex">
@@ -12,16 +13,90 @@
 				</div>
 			</div>
 			<div>
-				<p class="mx-40 mt-16 pb-16 text-3xl">GY.RA.VO en quelques mots ...</p>
+				<h2 class="mx-40 mt-16 pb-16 text-3xl montserrat">GY.RA.VO en quelques mots ...</h2>
 			</div>
 		</div>
 	</div>
+	<div>
+		<div class="flex justify-around mx-32 my-16">
+			<div v-for="(el,index) in values" :key="index">
+				<image-title-text :image="el.imgName" :title="el.title" :desc="el.description"></image-title-text>
+
+			</div>
+		</div>
+
+		<div class="mx-32">
+			<p class="my-5">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+			</p>
+
+			<p class="my-5">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+			</p>
+			<p class="my-5">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias, maxime sequi aliquam at qui illum itaque tempore porro dicta. Unde temporibus consequatur eius! Modi, ipsa.
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero aliquid, corporis architecto, deleniti molestias,
+			</p>
+		</div>
+
+		<div class="flex justify-around mx-32 my-16">
+			<div v-for="(el,index) in objectifs" :key="index">
+				<image-title-text :image="el.imgName" :title="el.title" :desc="el.description"></image-title-text>
+
+			</div>
+		</div>
+	</div>
+	<div>
+		<h2 class="mx-40 mt-16 pb-16 text-3xl montserrat">Les cours</h2>
+	</div>
+</div>
 </template>
 
 <script lang="ts">
+import ImageTitleText from "../components/ImageTitleText.vue"
 export default {
 	name: "IndexPage",
 	layout: "page",
+	components:{
+		ImageTitleText,
+	},
+
+	data(){
+		return{
+			values:[
+				{
+					imgName:"image-accueil.png",
+					title:"Valeur 1",
+					description:"Description de la valeur",
+				},
+				{
+					imgName:"image-accueil.png",
+					title:"Valeur 2",
+					description:"Description de la valeur",
+				},
+				{
+					imgName:"image-accueil.png",
+					title:"Valeur 3",
+					description:"Description de la valeur",
+				},
+			],
+			objectifs:[
+				{
+					imgName:"image-accueil.png",
+					title:"Objectif 1",
+					description:"Description de la valeur",
+				},
+				{
+					imgName:"image-accueil.png",
+					title:"Objectif 2",
+					description:"Description de la valeur",
+				},
+			]
+		}
+	}
 }
 </script>
 
