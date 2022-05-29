a9216efaf902f2c777ea15019db68b9414d2ff3b
Caroline <caroline@codekraft.fr>
2022-05-29 09:46:10 +0200

    Create components ImageTitleText

---
+++ components/ImageTitleText.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<img id="img" :src="require(`@/static/${image}`)" :class="`w-36 h-36 ${rounded ? 'rounded': null}`">
+		<div class="text-center my-2">
+			<p class="font-semibold">{{title}}</p>
+			<p class="text-sm">{{desc}}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props:{
+		image: {
+			type: String,
+			default: "a"
+		},
+		title: {
+			type: String,
+			default: "test"
+		},
+		desc: {
+			type: String,
+			default: "test"
+		},
+		rounded:{
+			type: Boolean,
+			default: false,
+		}
+	},
+	data(){
+		return{
+			img: document.getElementById('img')
+		}
+	},
+
+
+
+}
+</script>
+
+<style scoped>
+
+.rounded{
+	border-radius: 100%;
+}
+
+</style>
