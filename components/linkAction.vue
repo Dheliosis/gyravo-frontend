39e1eec3c195685617d5fb0ef3cda6ab9a182227
Caroline Fassot <fassot.caroline@gmail.com>
2022-06-06 16:01:51 +0200

    Create LinkAction

---
+++ components/linkAction.vue
@@ -0,0 +1,66 @@
+<template>
+	<div :class="`text-sm flex items-center cursor-pointer ${color}`">
+		<font-awesome-icon :icon="`${icon}`" class="mr-4"/>
+		<p class="underline">{{text}}</p>
+	</div>
+</template>
+
+<script>
+import icon from "../assets/icon"
+
+export default {
+
+	props:{
+		icon:{
+			type: String,
+			default: "none"
+		},
+		color:{
+			type: String,
+			default: "black",
+		},
+		text:{
+			type: String,
+			default: "Enter a text"
+		}
+	}
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/_variables.scss";
+
+div{
+	transition: 0.3s;
+}
+.green{
+	color: $mainGreen;
+	&:hover{
+		color: $mainBlue
+	}
+
+	&:active{
+		color: $tertiaryBlue
+	}
+}
+
+.violet{
+	color: $mainViolet;
+}
+
+.blue{
+	color: $mainBlue;
+}
+
+.black{
+	&:hover{
+		color: $mainGreen
+	}
+
+	&:active{
+		color: $tertiaryGreen,
+	}
+}
+
+</style>
